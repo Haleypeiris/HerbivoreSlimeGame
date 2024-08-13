9dc6b838443bd15784e80dd0f677d6eeac4902d9
unknown <haley.peiris@gmail.com>
2024-08-13 15:53:19 -0400

    Hid Debug Console

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,9 +1,17 @@
 #pragma once
 #include "Game.h"
+#include <Windows.h>
 
 int main()
 {
     Game game;
+
+    //Hides the console
+    HWND window;
+    AllocConsole();
+    window = FindWindowA("ConsoleWindowClass", NULL);
+    ShowWindow(window, 0);
+
 
     //Main Loop
     while (game.getWindowIsOpen()) 
